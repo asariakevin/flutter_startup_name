8668e57afcc2048d4cb308769ed7071327518d4d
asariakevin <asariakevin@gmail.com>
2019-07-24 12:16:17 +0300

    add icons to the list

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,64 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome To Flutter"),
-        ),
-        
-        body:  Center(
-//          child: const Text('Hello World'),
-            child:  RandomWords(),
-        ),
-      ),
+      home: RandomWords()
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords>{
 
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>(); //stores the word pairing that the user favorited
+  final TextStyle _biggerFont = const TextStyle( fontSize: 18);
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+
+        itemBuilder:(BuildContext _context,int i){
+
+          if(i.isOdd){
+            return Divider();
+          }
+
+
+          final int index = i ~/2 ;
+
+          if (index >= _suggestions.length){
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    final bool alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final WordPair wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    //final WordPair wordPair = WordPair.random();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Startup Name Generator"),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
 
